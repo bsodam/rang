500ae2c994dc08aa730d9a626dad72f41ecf01a6
Sodam Baek <bsodam@gmail.com>
2019-01-23 11:20:13 +0900

    [feature] detail page and search feature added

--- posts/urls.py
+++ posts/urls.py
@@ -4,4 +4,7 @@ from . import views
 app_name = 'posts'
 urlpatterns = [
     path('list/all/', views.ListView.as_view(), name='list'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('search_results/', views.SearchResultListView.as_view(), name='search_results'),
+
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView
 
 from posts.models import Post
 
@@ -12,3 +12,20 @@ class ListView(ListView):
 
     def get_queryset(self):
         return Post.objects.order_by('id')
+
+
+class DetailView(DetailView):
+    model = Post
+
+
+class CreateView(CreateView):
+    pass
+
+class SearchResultListView(ListView):
+    template_name = 'posts/list.html'
+    context_object_name = 'post_list'
+
+    def get_queryset(self):
+        search_keyword = self.request.GET.get('search_keyword', '')
+        return Post.objects.filter(title__contains=search_keyword)
+
